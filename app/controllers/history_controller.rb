1a9fad509e85934eeabc0664cee24e7381152dc3
john  kyony <jkyony@gmail.com>
2015-11-14 13:39:49 +0000

    History added

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -3,7 +3,7 @@ class HistoryController < ApplicationController
      # find user id to make sure its the current user
      user_id = User.find_by_id(current_user.id)
     # find  the current user expenses 
-    @expense_history = Expense.all.where(:user_id => user_id)
+    @expense_history = Expense.all.where(:user_id => user_id , :done => true)
     # find the current user expense history
     @expenses_history_balance = @expense_history.sum(:amount)
   end
