08c792199ea56033c5b470a1342ea5b17a3bef5d
john  kyony <jkyony@gmail.com>
2015-11-14 12:56:34 +0000

    History expense view history fix

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -1,7 +1,8 @@
 class HistoryController < ApplicationController
   def index
-    @expense_history = Expense.all.where(:done => true)
-    @expenses_balance = @expense_history.sum(:amount)
-    
+     # find user id to make sure its the current user
+     user_id = User.find_by_id(current_user.id)
+    # find  the current user expenses 
+    @expenses = Expense.all.where(:user_id => user_id , :done =>true)
   end
 end
