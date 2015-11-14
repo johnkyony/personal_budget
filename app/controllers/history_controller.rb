392631022d78280b408d4217cfeac2b03ee70dd0
john  kyony <jkyony@gmail.com>
2015-11-14 12:59:08 +0000

    History controller fix

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -3,6 +3,6 @@ class HistoryController < ApplicationController
      # find user id to make sure its the current user
      user_id = User.find_by_id(current_user.id)
     # find  the current user expenses 
-    @expenses = Expense.all.where(:user_id => user_id , :done =>true)
+    @expense_history = Expense.all.where(:user_id => user_id , :done =>true)
   end
 end
