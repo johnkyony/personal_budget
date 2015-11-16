d0e41c5ad5fa95bd59d1bfc6544229aa436eb2a8
john  kyony <jkyony@gmail.com>
2015-11-16 15:19:38 +0000

    history fix

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -2,10 +2,10 @@ class HistoryController < ApplicationController
   def index
      # find user id to make sure its the current user
      user_id = User.find_by_id(current_user.id)
-    # find the current user wallet
+    # find the current user wallte
     current_user_wallet = Wallet.all.where(:user_id => user_id).pluck(:id)
     # find  the current user expenses 
-    @expense_history = Expense.all.where(:user_id => user_id , :done => true , :wallet_id => current_user_wallet)
+    @expense_history = Expense.all
     # find the current user expense history
     @expenses_history_balance = @expense_history.sum(:amount)
   end
