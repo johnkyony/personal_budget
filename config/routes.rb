122de60c1ff4df9d8b8096e047c91d1925e7f1ab
john  kyony <jkyony@gmail.com>
2015-11-20 16:50:55 +0000

    User log in

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   get "/callback" => "facebook#callback"
   get "/facebook_profile" => "facebook#facebook_profile"
  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+ devise_scope :user do
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
   resources :users
   root to: 'wallets#index'
 
