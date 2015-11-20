84205f492e97772a0f1161991de50a506896c451
john  kyony <jkyony@gmail.com>
2015-11-20 13:03:35 +0000

    Remove facebook oauth setting

--- app/models/facebook_oauth_setting.rb
+++
@@ -1,2 +0,0 @@
-class FacebookOauthSetting < ActiveRecord::Base
-end
--- test/models/facebook_oauth_setting_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe FacebookOauthSetting do
-  let(:facebook_oauth_setting) { FacebookOauthSetting.new }
-
-  it "must be valid" do
-    value(facebook_oauth_setting).must_be :valid?
-  end
-end
